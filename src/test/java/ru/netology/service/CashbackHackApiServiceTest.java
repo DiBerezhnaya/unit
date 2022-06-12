e344dca9a360899284855fea714447713af0ed00
Диана Бережная <Lacshri@yandex.ru>
2022-06-12 22:43:40 +0300

    junit5 and legacy

---
+++ src/test/java/ru/netology/service/CashbackHackApiServiceTest.java
@@ -0,0 +1,42 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CashbackHackApiServiceTest {
+
+     @Test
+        public void equalToTheAmount() {
+            CashbackHackService service = new CashbackHackService();
+            int amount = 1000;
+
+            int actual = service.remain(amount);
+            int expected =0;
+
+            assertEquals(actual, expected);
+        }
+
+        @Test
+        public void moreThanTheAmount() {
+            CashbackHackService service = new CashbackHackService();
+            int amount = 1100;
+
+            int actual = service.remain(amount);
+            int expected = 900;
+
+            assertEquals(actual, expected);
+        }
+
+        @Test
+        public void lessThanTheAmount() {
+            CashbackHackService service = new CashbackHackService();
+            int amount = 900;
+
+            int actual = service.remain(amount);
+            int expected = 100;
+
+            assertEquals(actual, expected);
+     }
+
+}
