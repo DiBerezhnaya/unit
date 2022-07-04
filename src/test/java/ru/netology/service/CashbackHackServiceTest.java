cc56de649c295569950f8432b537600693ca717d
Диана Бережная <Lacshri@yandex.ru>
2022-07-04 20:39:24 +0300

    added failed test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,6 +7,17 @@ import static org.testng.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
+    public void equalToTheAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
     public void moreThanTheAmount() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1100;
